768fe9d43dfe87138f1177eee60c70f8a4389f34
Rhys Rushton <rhys.rushton@gmail.com>
2020-07-16 12:44:19 +1000

    building about page

---
+++ hello-world/src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function about(){
+    return(
+        <div>
+            <h1>Hey</h1>
+            <p>My name is rhys </p>
+        </div>
+    )
+
+
+
+
+}
